f4af795f8b1cff001d0b672e1f9f5198bff0cbab
naresh-coditas <39824227+naresh-coditas@users.noreply.github.com>
2019-09-04 01:42:58 +0530

    Delete IDataStore.java

--- src/test/java/com/tavisca/test/datastore/IDataStore.java
+++
@@ -1,9 +0,0 @@
-package com.tavisca.test.datastore;
-
-public interface IDataStore<T> {
-	public T getRecord(Integer id);
-
-	public  T saveRecord(T value);
-
-	public T removeRecord(Integer id);
-}
